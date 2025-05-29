5a6343c39aa5f8a3678ca5f32f5a9a3d37c8ddc6
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-28 19:41:03 -0700

    feat: session-persistent tab renaming

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,6 +8,19 @@ local function picker_open()
   return P and #P.get() > 0
 end
 
+-- setup :BufferLineRename
+vim.api.nvim_create_user_command("BufferLineRename", function(opts)
+  local current_tab = vim.api.nvim_get_current_tabpage()
+  if opts.args == "" then
+    vim.g["BufferLineCustomName" .. current_tab] = nil
+  else
+    vim.g["BufferLineCustomName" .. current_tab] = opts.args
+  end
+end, {
+  nargs = "?",
+  desc = "Rename the current tab",
+})
+
 return {
   "n-crespo/bufferline.nvim",
   event = "VeryLazy",
@@ -22,7 +35,7 @@ return {
       show_duplicate_prefix = true,
       default_duplicate_prefix = "",
       show_tab_indicators = false,
-      always_show_bufferline = false,
+      always_show_bufferline = true,
       show_close_icon = false,
       show_buffer_close_icons = false,
       show_buffer_icons = false,
@@ -31,6 +44,15 @@ return {
       modified_icon = "",
       separator_style = { "", "" },
       style_preset = require("bufferline").style_preset.no_italic,
+
+      name_formatter = function(buf)
+        local custom_name = vim.g["BufferLineCustomName" .. buf.tabnr]
+        if custom_name and custom_name ~= "" then
+          return custom_name
+        else
+          return buf.name
+        end
+      end,
 
       numbers = function(opts)
         -- use the :h tabpagenr() instead of :h tabpageid
@@ -133,6 +155,18 @@ return {
       { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
       { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
       { "<leader>p", require("bufferline").pick },
+      {
+        "<leader>r",
+        function()
+          local current_tab = vim.api.nvim_get_current_tabpage()
+          vim.ui.input({ prompt = "New Tab Name: " }, function(input)
+            if input or input == "" then
+              vim.g["BufferLineCustomName" .. current_tab] = input
+            end
+          end)
+        end,
+        desc = "Rename Tab",
+      },
       {
         "<A-,>",
         function()
