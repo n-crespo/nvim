510a016d1404e89baf391b24d421e1e87a80297b
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-06-07 12:57:05 -0700

    feat: stiffen smear

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -6,11 +6,11 @@ return {
     smear_insert_mode = true,
     legacy_computing_symbols_support = true,
 
-    stiffness = 0.8, -- 0.6      [0, 1]
-    trailing_stiffness = 0.5, -- 0.4      [0, 1]
+    stiffness = 0.8, -- 0.5      [0, 1]
+    trailing_stiffness = 0.7, -- 0.4      [0, 1]
     time_interval = 7, -- milliseconds
-    stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
-    trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+    stiffness_insert_mode = 0.8, -- 0.6      [0, 1]
+    trailing_stiffness_insert_mode = 0.8, -- 0.6      [0, 1]
     distance_stop_animating = 0.5, -- 0.1      > 0
 
     never_draw_over_target = true,
