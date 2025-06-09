3342b148a2ee194abc31747d14b3553efcb3e3f4
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-06-08 23:30:40 -0700

    chore: remove old custom tabline code for highlight gen

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -29,34 +29,5 @@ return {
         c = { glyph = "", hl = "MiniIconsBlue" },
       },
     },
-    config = function(_, opts)
-      require("mini.icons").setup(opts)
-
-      -- generate custom highlights that are used in tabline (see lualine.lua)
-      local highlights = {
-        "MiniIconsAzure",
-        "MiniIconsBlue",
-        "MiniIconsCyan",
-        "MiniIconsGreen",
-        "MiniIconsGrey",
-        "MiniIconsOrange",
-        "MiniIconsPurple",
-        "MiniIconsRed",
-        "MiniIconsYellow",
-      }
-
-      local hl_get = function(name)
-        return vim.api.nvim_get_hl(0, { name = name, link = false })
-      end
-
-      local tabs_active = hl_get("TabLineSel")
-      local tabs_inactive = hl_get("TabLineFill")
-
-      for _, hl in ipairs(highlights) do
-        local icon = hl_get(hl)
-        vim.api.nvim_set_hl(0, hl .. "_lualine_a_tabs_active", { fg = icon.fg, bg = tabs_active.bg })
-        vim.api.nvim_set_hl(0, hl .. "_lualine_a_tabs_inactive", { fg = icon.fg, bg = tabs_inactive.bg })
-      end
-    end,
   },
 }
