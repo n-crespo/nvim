722bf82948a86664850f3ee7461deb8ab0672d8f
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-06-10 13:54:34 -0700

    fix: properly toggle terminals

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -17,41 +17,23 @@ return {
     {
       "<C-q>",
       function()
-        Snacks.terminal.toggle()
+        require("snacks.terminal").toggle(nil, {
+          win = { position = "bottom" },
+          env = { SNACKS_TERM_ID = "bottom_terminal" },
+        })
       end,
-      desc = "Toggle Terminal",
+      desc = "Toggle bottom terminal",
       mode = { "n", "t" },
     },
     {
-      "<C-S-/>",
+      "<C-s-/>",
       function()
-        Snacks.terminal.toggle(vim.g.shell, {
-          win = {
-            position = "float",
-            height = 0.8,
-            width = 0.8,
-            border = "rounded",
-          },
+        require("snacks.terminal").toggle(nil, {
+          win = { position = "float", border = "rounded" },
+          env = { SNACKS_TERM_ID = "floating_terminal" },
         })
       end,
-      desc = "Toggle Floating Terminal",
-      mode = { "n", "t" },
-    },
-    {
-      "<C-S-Q>",
-      function()
-        Snacks.terminal.toggle(vim.g.shell, {
-          win = {
-            relative = "editor",
-            position = "float",
-            height = 0.8,
-            width = 0.8,
-            border = "rounded",
-          },
-          -- cwd = vim.loop.cwd(),
-        })
-      end,
-      desc = "Toggle Floating Terminal",
+      desc = "Toggle floating terminal",
       mode = { "n", "t" },
     },
   },
