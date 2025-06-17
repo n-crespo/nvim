0c6dfd6d4b7b9559be373dfe328e57b95be3a36d
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-06-17 14:47:39 -0400

    fix(flash): remove winborder and use fuzzy searching

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -3,16 +3,27 @@ return {
   enabled = true,
   event = "VeryLazy",
   opts = {
-    search = { multi_window = false },
+    search = {
+      mode = "fuzzy",
+      multi_window = false,
+    },
     label = { uppercase = false },
     jump = {
       nohlsearch = true, -- clear highlight after jump
       autojump = false, -- automatically jump when there is only one match
     },
+    highlight = {
+      backdrop = false, -- do not dim the background
+    },
     modes = {
       -- `f`, `F`, `t`, `T`, `;` and `,` motions
       char = { enabled = false },
       search = { enabled = false },
+    },
+    prompt = {
+      win_config = {
+        border = "none",
+      },
     },
   },
 }
