3f13f4fd8d9a2696778e35e47da6698f82c5350f
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-06-18 19:51:21 -0400

    fix(smear): don't animate small movements to prevent input delay

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -11,7 +11,7 @@ return {
     time_interval = 7, -- milliseconds
     stiffness_insert_mode = 0.8, -- 0.6      [0, 1]
     trailing_stiffness_insert_mode = 0.6, -- 0.6      [0, 1]
-    distance_stop_animating = 0.1, -- 0.1      > 0
+    distance_stop_animating = 0.5, -- 0.1      > 0
 
     never_draw_over_target = true,
     hide_target_hack = false,
