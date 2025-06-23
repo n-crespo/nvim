9443e9250d5030c122002009d437e4916e6ccc5b
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-06-22 21:43:09 -0400

    fix: disable copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,9 +26,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml", cond = full_config },
     { import = "lazyvim.plugins.extras.lang.yaml", cond = full_config },
     { import = "lazyvim.plugins.extras.lang.typescript", cond = full_config },
-    { import = "lazyvim.plugins.extras.ai.copilot", cond = full_config },
     { import = "lazyvim.plugins.extras.ai.copilot-chat", cond = full_config },
     -- { import = "lazyvim.plugins.extras.ui.smear-cursor", cond = full_config },
+    -- { import = "lazyvim.plugins.extras.ai.copilot", cond = full_config },
     -- { import = "lazyvim.plugins.extras.lang.java", cond = full_config },
 
     -- this enables user define plugins, from ../plugins/
