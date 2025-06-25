fea8532dd863c0d2bae8150c6b7d633ac86b32a2
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-06-25 14:47:37 -0400

    fix(pvs): set fdm option locally only

--- after/ftplugin/pvs.lua
+++ after/ftplugin/pvs.lua
@@ -3,5 +3,5 @@ if not vim.g.vscode then
   local ft = require("Comment.ft")
   ft.pvs = "% %s"
   -- vim.opt_local.foldmethod = "indent"
-  vim.cmd([[set fdm=indent]])
+  vim.cmd([[setlocal fdm=indent]])
 end
