1522b401a55df1a604cc0a620eec6fb6e9342167
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-06-26 17:24:35 -0400

    feat(ft): improve keymaerax highlighting

---
+++ after/ftdetect/keymaera.lua
@@ -0,0 +1,7 @@
+-- use haskell highlighting in .kyx files and "keymaerax" codeblocks
+vim.filetype.add({
+  extension = {
+    kyx = "keymaerax",
+  },
+})
+vim.treesitter.language.register("haskell", "keymaerax")
