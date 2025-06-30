14bfc13d23d933aed786c57f173172a4edc30b5f
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-06-30 10:00:20 -0400

    fix(smear): make it faster

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -4,20 +4,22 @@ return {
     anticipation = 0,
     damping = 0.99, -- setting this to 1 is a bit buggy
     cursor_color = "#c9c9c9",
-    transparent_bg_fallback_color = "#1D1D1D",
+    transparent_bg_fallback_color = "#0D0D0D",
     smear_between_neighbor_lines = true,
-    smear_insert_mode = true,
     legacy_computing_symbols_support = true,
-    legacy_computing_symbols_support_vertical_bars = false,
+    legacy_computing_symbols_support_vertical_bars = true,
     gamma = 1,
 
     stiffness = 0.8, -- 0.5      [0, 1]
     trailing_stiffness = 0.5, -- 0.4      [0, 1]
+
+    smear_insert_mode = true,
     stiffness_insert_mode = 0.8, -- 0.6      [0, 1]
-    trailing_stiffness_insert_mode = 0.6, -- 0.6      [0, 1]
-    distance_stop_animating = 0.5, -- 0.1      > 0
+    trailing_stiffness_insert_mode = 0.8, -- 0.6      [0, 1]
+    damping_insert_mode = 0.99, -- setting this to 1 is a bit buggy
 
     -- time_interval = 6, -- milliseconds
+    distance_stop_animating = 0.5, -- 0.1      > 0
     never_draw_over_target = true,
     hide_target_hack = true,
   },
