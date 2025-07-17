f804615033e8eb54c4bef9601c32c9b14fa6993d
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-07-16 22:44:20 -0400

    fix(python): ignore blank space at EOL error

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -6,7 +6,7 @@ if package.loaded["lspconfig"] and vim.g.full_config then
           pycodestyle = {
             -- this disables errors about lines being too long,
             -- line breaks between binary operators
-            ignore = { "E501", "W503" },
+            ignore = { "E501", "W503", "W391" },
           },
         },
       },
