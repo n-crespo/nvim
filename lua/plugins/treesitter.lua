4932321707ac2ff9a9138e06c883a7b1320860d4
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-07-17 00:55:17 -0400

    perf(treesitter): remove redundant installation checks

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,42 +9,39 @@
 -- })
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = "LazyFile",
-  opts = {
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "v<cr>",
-        scope_incremental = "<tab>",
-        node_incremental = "<cr>",
-        node_decremental = "<S-CR>",
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<BS>", desc = "Decrement Selection", mode = "x" },
+        { "<CR>", desc = "Increment Selection", mode = { "x", "n" } },
       },
     },
-    ensure_installed = {
-      "diff",
-      "bash",
-      "csv",
-      "yaml",
-      "xml",
-      "vimdoc",
-      "css",
-      "objdump",
-      "erlang",
-      "comment",
-      "ssh_config",
-      "editorconfig",
-      "json",
-      "jsonc",
-      "json5",
-      "matlab",
-      "markdown_inline",
-      "regex",
-      "vim",
-      "query",
-      "html",
-      "vimdoc",
-      "todotxt",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "LazyFile",
+    opts = {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "v<cr>",
+          node_incremental = "<cr>",
+          node_decremental = "<S-CR>",
+        },
+      },
+      ensure_installed = {
+        "comment",
+        "css",
+        "csv",
+        "editorconfig",
+        "erlang",
+        "fish",
+        "git_config",
+        "objdump",
+        "ssh_config",
+        "todotxt",
+      },
     },
   },
 }
