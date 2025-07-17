e7c89d741a102b416bc1c2154254802174464aef
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-07-17 00:41:42 -0400

    feat(ft): establish pvs filetype with autocmd

--- after/ftdetect/pvs.lua
+++ after/ftdetect/pvs.lua
@@ -1,5 +1,12 @@
-vim.filetype.add({
-  extension = { pvs = "pvs" },
-  filename = { ["pvs-strategies"] = "lisp" },
+vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPre" }, {
+  pattern = { "*.pvs", "pvs-strategies" },
+  group = vim.api.nvim_create_augroup("keymaerax_ftdetect", { clear = true }),
+  once = true,
+  callback = function()
+    vim.filetype.add({
+      extension = { pvs = "pvs" },
+      filename = { ["pvs-strategies"] = "lisp" },
+    })
+    vim.treesitter.language.register("erlang", "pvs")
+  end,
 })
-vim.treesitter.language.register("erlang", "pvs")
