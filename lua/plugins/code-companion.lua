a935d544435c26a4e8a76f39a2b55c3b519a76b1
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-08-25 11:07:00 -0400

    feat: improve code-companion UX

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -1,6 +1,50 @@
-local spinner
 vim.cmd([[cab cc CodeCompanion]]) -- works in visual mode too!
 vim.cmd([[cab cmd CodeCompanionCmd]])
+
+-- https://github.com/olimorris/codecompanion.nvim/discussions/813#discussioncomment-13081665
+local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", { clear = true })
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "CodeCompanion*",
+  group = group,
+  callback = function(request)
+    if
+      request.match == "CodeCompanionChatSubmitted"
+      or request.match == "CodeCompanionContextChanged"
+      or request.match == "CodeCompanionChatCreated"
+      or request.match == "CodeCompanionChatOpened"
+      or request.match == "CodeCompanionChatModel"
+      or request.match == "CodeCompanionChatAdapter"
+      or request.match == "CodeCompanionChatClosed"
+      or request.match == "CodeCompanionChatHidden"
+    then
+      return
+    end
+    local msg
+    msg = "[CodeCompanion] " .. request.match:gsub("CodeCompanion", "")
+    vim.notify(msg, "info", {
+      timeout = 1000,
+      keep = function()
+        return not vim
+          .iter({ "Finished", "Opened", "Hidden", "Closed", "Cleared", "Created" })
+          :fold(false, function(acc, cond)
+            return acc or vim.endswith(request.match, cond)
+          end)
+      end,
+      id = "code_companion_status",
+      title = "Code Companion Status",
+      opts = function(notif)
+        notif.icon = ""
+        if vim.endswith(request.match, "Started") then
+          ---@diagnostic disable-next-line: undefined-field
+          notif.icon = spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+        elseif vim.endswith(request.match, "Finished") then
+          notif.icon = " "
+        end
+      end,
+    })
+  end,
+})
 
 return {
   "olimorris/codecompanion.nvim",
@@ -8,7 +52,33 @@ return {
   dependencies = { "saghen/blink.cmp" },
   cmd = "CodeCompanion", -- allow the abbreviation :cc to load the plugin
   config = true,
+  init = function()
+    vim.api.nvim_create_autocmd({ "User" }, {
+      pattern = "CodeCompanionInlineFinished",
+      group = group,
+      callback = function(request)
+        vim.lsp.buf.format({ bufnr = request.buf })
+      end,
+    })
+  end,
   opts = {
+    display = {
+      chat = {
+        intro_message = "  What can I help with? (Press ? for options)",
+        show_references = true,
+        show_header_separator = false,
+        show_settings = true,
+        window = {
+          width = 0.4,
+          opts = {
+            relativenumber = false,
+            number = false,
+            statuscolumn = "  ",
+          },
+        },
+      },
+    },
+
     strategies = {
       chat = {
         adapter = "copilot",
@@ -40,22 +110,12 @@ return {
       inline = { adapter = "copilot" },
       cmd = { adapter = "copilot" },
     },
-    display = {
-      chat = {
-        window = {
-          opts = {
-            number = false,
-            statuscolumn = "  ",
-          },
-        },
-      },
-    },
   },
   keys = {
     {
       "<leader>k",
-      "<cmd>CodeCompanion<cr>",
-      desc = "Prompt Code Companion",
+      ":CodeCompanion<cr>",
+      desc = "Prompt AI",
       mode = { "n", "x" },
     },
     {
@@ -63,7 +123,7 @@ return {
       function()
         require("codecompanion").chat()
       end,
-      desc = "CodeCompanion Chat",
+      desc = "AI Chat",
       mode = "n",
     },
     {
@@ -73,7 +133,7 @@ return {
         vim.cmd([[normal! o]])
         vim.cmd([[normal! o]])
       end,
-      desc = "Add to CodeCompanion Chat",
+      desc = "Add to New AI Chat",
       mode = "v",
     },
     {
@@ -83,7 +143,7 @@ return {
         require("codecompanion").add()
       end,
       mode = "v",
-      desc = "Add to CodeCompanion Chat",
+      desc = "Add to Existing AI Chat",
     },
   },
 }
