b9273132ef7b26ba851ccecb62a583fb8c428246
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-08-26 23:58:06 -0400

    chore: removed old code

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,56 +2,6 @@
 
 local ignored_bt = { prompt = true, nofile = true, terminal = true, quickfix = true }
 
--- local function updateTitleString(ev)
---   if #vim.api.nvim_list_tabpages() == 1 then
---     -- don't try to update if its not needed!
---     local name
---     if vim.api.nvim_get_option_value("filetype", { scope = "local" }) == "snacks_dashboard" then
---       name = "nvim"
---     elseif vim.api.nvim_get_option_value("filetype", { scope = "local" }) == "checkhealth" then
---     elseif
---       ignored_bt[vim.api.nvim_get_option_value("buftype", { scope = "local" })]
---       or vim.api.nvim_get_option_value("bufhidden", { scope = "local" }) ~= ""
---       or vim.api.nvim_buf_get_name(ev.buf) == ""
---     then
---       return
---     else
---       local custom_name = vim.g["BufferLineCustomName" .. 1]
---       name = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
---       if custom_name and custom_name ~= "" then
---         name = custom_name
---       end
---       if name == "" then
---         name = ":checkhealth" -- i think this only happens in health buffers
---       end
---     end
---
---     if name == "" then
---       name = "nvim"
---     else
---       name = vim.fn.fnamemodify(name, ":t")
---     end
---     vim.o.titlestring = name
---   end
--- end
-
--- HACK: bufferline doesn't update the titlestring when the tabline is hidden
--- (when only one tabpage is open)... so i have this largely redundant autocmd
--- to fix the titlestring when bufferline is unreliable
--- vim.api.nvim_create_autocmd({ "VimEnter", "TabNew", "TabClosed", "BufEnter", "BufWritePost", "User" }, {
---   desc = "Update titlestring with buffer name if only one tabpage is open",
---   callback = function(ev)
---     updateTitleString(ev)
---   end,
--- })
--- vim.api.nvim_create_autocmd({ "User" }, {
---   desc = "Update titlestring with buffer name if only one tabpage is open",
---   pattern = "BufferLineRenamed",
---   callback = function(ev)
---     updateTitleString(ev)
---   end,
--- })
-
 vim.api.nvim_create_user_command("BufferLineRename", function(opts)
   local current_tab = vim.api.nvim_get_current_tabpage()
   if opts.args == "" then
