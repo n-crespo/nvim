85674673e31c50a39910b988c9cdc2e28a14aa65
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-08-29 18:58:56 -0400

    fix(pvs): fold with indent

--- after/ftplugin/pvs.lua
+++ after/ftplugin/pvs.lua
@@ -2,6 +2,6 @@ vim.opt.commentstring = "% %s"
 if not vim.g.vscode then
   local ft = require("Comment.ft")
   ft.pvs = "% %s"
-  -- vim.opt_local.foldmethod = "indent"
+  vim.opt_local.foldmethod = "indent"
   vim.cmd([[setlocal fdm=indent]])
 end
