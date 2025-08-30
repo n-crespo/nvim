dbc60d36501310039af4ad9f9e912003c0f37160
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-08-29 22:38:21 -0400

    feat: add mini.move

---
+++ lua/plugins/mini-move.lua
@@ -0,0 +1,36 @@
+return {
+  "nvim-mini/mini.move",
+  opts = {
+    mappings = {
+      left = "<M-[>",
+      right = "<M-]>",
+      down = "<C-N>",
+      up = "<C-P>",
+      line_left = "<M-[>",
+      line_right = "<M-]>",
+      line_down = "<C-N>",
+      line_up = "<C-P>",
+    },
+    options = {
+      reindent_linewise = true,
+    },
+  },
+  keys = {
+    {
+      "<M-]>",
+      function()
+        require("mini.move").move_line("right")
+      end,
+      mode = "i",
+      desc = "Indent line",
+    },
+    {
+      "<M-[>",
+      function()
+        require("mini.move").move_line("left")
+      end,
+      mode = "i",
+      desc = "Un-indent line",
+    },
+  },
+}
