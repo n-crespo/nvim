6949d9268ccef5ac2333c2ecc112c98f2651b01f
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-09-07 00:02:01 -0400

    chore: clean up smear config

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -3,11 +3,9 @@ return {
   opts = {
     anticipation = 0,
     damping = 0.999, -- setting this to 1 is a bit buggy
-    cursor_color = "#c9c9c9",
+    cursor_color = "#e2e2e2",
     transparent_bg_fallback_color = "#0D0D0D",
     smear_between_neighbor_lines = true,
-    legacy_computing_symbols_support = true,
-    legacy_computing_symbols_support_vertical_bars = true,
     gamma = 1,
 
     time_interval = 7, -- milliseconds, default 17
@@ -18,11 +16,11 @@ return {
     smear_insert_mode = true,
     stiffness_insert_mode = 0.8, -- 0.6      [0, 1]
     trailing_stiffness_insert_mode = 0.8, -- 0.6      [0, 1]
-    damping_insert_mode = 0.99, -- setting this to 1 is a bit buggy
+    damping_insert_mode = 0.999, -- setting this to 1 is a bit buggy
 
     -- time_interval = 6, -- milliseconds
     distance_stop_animating = 0.5, -- 0.1      > 0
-    never_draw_over_target = true,
+    never_draw_over_target = false,
     hide_target_hack = true,
     smear_to_cmd = false,
   },
