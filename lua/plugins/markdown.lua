67efbf50ca080308260d2ff40ccf0bd5fe0ba31b
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-09-20 02:11:56 -0400

    fix(preview): use zen browser to preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -89,7 +89,10 @@ local M = {
       }
 
       if vim.fn.has("wsl") == 1 then
-        opts.app = { "/mnt/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe", "--new-window" }
+        -- opts.app = { "/mnt/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe", "--new-window" }
+        opts.app = { "/mnt/c/Program Files/Zen Browser/zen.exe", "--new-window" }
+      else
+        opts.app = { "zen" }
       end
 
       return opts
