d572507c55c77a475390578eaf9c8805752e61cc
Nicolas Crespo <ncrespo@ucla.edu>
2025-10-06 00:28:48 -0700

    fix(md): still use mdslw in full config

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -167,7 +167,7 @@ then
       "mfussenegger/nvim-lint",
       opts = {
         linters_by_ft = {
-          markdown = { "markdownlint-cli2" },
+          ["markdown"] = { "mdslw", "markdownlint-cli2" },
         },
         linters = {
           ["markdownlint-cli2"] = {
