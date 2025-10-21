34300ceb462fc5c9d25cd444e74f3430197678bb
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-10-21 00:25:23 -0700

    feat(markdown): use <TAB> to indent lines when cmp not active

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -5,6 +5,7 @@
 -- NOTE; these save to the `r` register arbitrarily as to not clog up system
 -- clipboard bold/italicize in visual mode
 vim.keymap.set("i", "<C-b>", "****<left><left>", { buffer = true, silent = true })
+vim.keymap.set("i", "<tab>", "<C-t>", { buffer = true, silent = true })
 vim.keymap.set("i", "<S-tab>", "<C-d>", { buffer = true, silent = true })
 vim.keymap.set("x", "<C-b>", '"rc****<esc>h"rP', { buffer = true, desc = "Bold" })
 vim.keymap.set("x", "<C-i>", '"rc__<esc>"rP', { buffer = true, desc = "Italicize" })
