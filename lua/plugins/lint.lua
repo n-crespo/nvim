1a2b070f10fef345ca932bc0ed3f20e4449579de
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-10-22 03:15:33 -0700

    feat(lint): use fallback linter for typos and properly setup oxlint

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    event = "LazyFile",
+    opts = {
+      linters_by_ft = {
+        fish = { "fish" },
+        javascript = { "oxlint" },
+        javascriptreact = { "oxlint" },
+        ["*"] = { "typos" },
+        -- Use the "*" filetype to run linters on all filetypes.
+        -- ['*'] = { 'global linter' },
+        -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+        -- ['_'] = { 'fallback linter' },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    event = "LspAttach",
+    opts = {
+      ui = {
+        border = "rounded",
+        width = 0.8,
+        height = 0.8,
+      },
+      ensure_installed = { "typos" },
+    },
+  },
+}
