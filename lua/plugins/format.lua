22b3906cee6468aa6b4093e6ab7fa1e2881d1514
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-11-11 02:24:01 -0800

    fix(format): centralize and simplify formatters

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,8 +1,17 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      ["html"] = { "superhtml" },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["html"] = { "superhtml" },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    event = "LspAttach",
+    opts = {
+      ensure_installed = { "superhtml" },
     },
   },
 }
