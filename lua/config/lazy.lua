ec6aae7dbe89370ea81a64f7b0373a029ae0452f
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-11-22 02:07:48 -0800

    fix(lazy): remove unneeded rest client, just use insomnia

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -35,7 +35,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript", cond = full_config },
     { import = "lazyvim.plugins.extras.lang.typst", cond = full_config },
     { import = "lazyvim.plugins.extras.linting.eslint", cond = full_config },
-    { import = "lazyvim.plugins.extras.util.rest", cond = full_config },
     -- { import = "lazyvim.plugins.extras.dap.core", cond = full_config },
     -- { import = "lazyvim.plugins.extras.ai.copilot-chat", cond = full_config },
     -- { import = "lazyvim.plugins.extras.ui.smear-cursor", cond = full_config },
