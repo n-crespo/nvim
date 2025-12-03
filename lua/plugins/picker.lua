87a79066f7030d4193ffef47be7d618d96769894
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-12-03 03:21:05 -0800

    feat(picker): specify sqlite lib path

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -2,6 +2,9 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      db = {
+        sqlite3_path = "/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6",
+      },
       sources = { files = { hidden = true, ignored = false } },
       formatters = {
         file = {
