1bb9a429ef4d265d5745f03f0f61e988468e398e
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-12-24 00:26:35 -0500

    fix(neovide): organize config

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -1,17 +1,14 @@
 if vim.g.neovide then
+  -- font and spacing
+  -- h10.2 is the font size , w-1 tightens width
+  vim.o.guifont = "JetBrainsMono NF:h10.2:w-1"
+  vim.opt.linespace = 5
+
+  -- zoom/scaling logic
   local font_size_factor = 1.1
   local change_font_size = function(factor)
     vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * factor
   end
-
-  -- macOS-specific, see if you like it compared to neovide_fullscreen.
-  vim.g.neovide_fullscreen = false
-
-  -- Replace it with any font you like. `h12` is the font size.
-  vim.o.guifont = "JetBrainsMono NF:h10.2:w-1"
-  vim.opt.linespace = 5
-
-  -- zoom keymaps
   vim.keymap.set("n", "<c-=>", function()
     change_font_size(font_size_factor)
   end)
@@ -22,29 +19,32 @@ if vim.g.neovide then
     vim.g.neovide_scale_factor = 1
   end)
 
+  -- refresh rate
+  vim.g.neovide_refresh_rate = 120
+  vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_no_idle = false
+  vim.g.neovide_confirm_quit = true
+
+  -- animation timings (shorter = snappier)
+  -- at 120Hz, 0.06 - 0.08s feels instant but smooth
+  vim.g.neovide_cursor_animation_length = 0.07
+  vim.g.neovide_cursor_trail_size = 0.2 -- smaller trail reduces visual clutter/lag feel
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_smooth_blink = true
+  vim.g.neovide_no_multigrid = true
+
   -- padding
   vim.g.neovide_padding_top = 7
   vim.g.neovide_padding_bottom = 1
   vim.g.neovide_padding_right = 3
   vim.g.neovide_padding_left = 3
 
-  -- Helper function for transparency formatting
-  -- g:neovide_opacity should be 0 if you want to unify transparency of content and title bar.
-  -- vim.g.neovide_opacity = 1
-  -- vim.g.transparency = 0
+  -- other visuals
   vim.g.neovide_background_color = "#0F0F0F"
-  vim.g.neovide_hide_mouse_when_typing = true
-
   vim.g.neovide_title_background_color = "#0F0F0F"
   vim.g.neovide_floating_corner_radius = 0.2
   vim.g.neovide_show_border = true
-  vim.g.experimental_layer_grouping = false
-  vim.g.neovide_refresh_rate = 120
-  vim.g.neovide_refresh_rate_idle = 5
-  vim.g.neovide_cursor_animation_length = 0.1
-  vim.g.neovide_scroll_animation_length = 0.2
-  vim.g.neovide_cursor_trail_size = 1.0
-  vim.g.neovide_cursor_animate_command_line = false
-  vim.g.neovide_cursor_smooth_blink = true
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_throttled = true
 end
 return {}
