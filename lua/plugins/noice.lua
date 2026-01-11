35a0d36664c10a8738556d651261b5188fa8804b
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-01-11 02:57:37 -0800

    fix(noice): prevent terminal resizing by nui cleanup/setup functions

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,8 +3,10 @@
 
 return {
   "folke/noice.nvim",
+  lazy = false, -- required to avoid weird terminal resizing
   opts = {
     presets = { lsp_doc_border = true },
+    cmdline = { view = "cmdline" },
     notify = { enabled = false },
     views = {
       mini = { win_options = { winblend = 0 } }, -- transparent lsp progress window
@@ -20,6 +22,20 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    local noice = require("noice")
+    noice.setup(opts)
+
+    -- HACK: prevents Noice from sending "cleanup" signals on exit that resize
+    -- terminal (related to windows terminal bug)
+    local original_disable = noice.disable
+    noice.disable = function()
+      if vim.v.exiting ~= vim.NIL then
+        return
+      end
+      original_disable()
+    end
+  end,
   keys = {
     { "<leader>snt", nil },
     { "<leader>sna", nil },
