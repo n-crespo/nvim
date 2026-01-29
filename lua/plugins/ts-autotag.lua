fa61f60fe03741f98d58396291d0c655850d6ade
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-01-28 23:59:04 -0800

    perf: lazy load ts-autotag plugin

---
+++ lua/plugins/ts-autotag.lua
@@ -0,0 +1,28 @@
+return {
+  "windwp/nvim-ts-autotag",
+  event = function()
+    return {}
+  end,
+  optional = true,
+  ft = {
+    "astro",
+    "typescriptreact",
+    "dot",
+    "glimmer",
+    "handlebars",
+    "html",
+    "javascript",
+    "jsx",
+    "liquid",
+    "vento",
+    "markdown",
+    "php",
+    "rescript",
+    "svelte",
+    "tsx",
+    "twig",
+    "typescript",
+    "vue",
+    "xml",
+  },
+}
