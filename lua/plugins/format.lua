a6cc0854860c2df73c6ab6325bb03027dcfb3725
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-01-28 23:41:58 -0800

    chore: remove unused configs

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -2,23 +2,16 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      formatters_by_ft = {
-        ["c"] = { "clang-format" },
-        -- ["html"] = { "superhtml" },
-        -- ["zsh"] = { "beautysh" },
-      },
+      -- formatters_by_ft = {
+      -- ["c"] = { "clang-format" },
+      -- ["html"] = { "superhtml" },
+      -- ["zsh"] = { "beautysh" },
+      -- },
       formatters = {
         beautysh = {
           append_args = { "--indent-size", "2" },
         },
       },
-    },
-  },
-  {
-    "mason-org/mason.nvim",
-    event = "LspAttach",
-    opts = {
-      -- ensure_installed = { "superhtml" },
     },
   },
 }
