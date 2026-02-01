7705e421d93a36f54888716a1d14b760cf335797
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-01-31 18:17:00 -0800

    chore(code-companion): organize config

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -1,66 +1,8 @@
-vim.cmd([[cab cc CodeCompanion]]) -- works in visual mode too!
--- vim.cmd([[cab cmd CodeCompanionCmd]])
-
--- https://github.com/olimorris/codecompanion.nvim/discussions/813#discussioncomment-13081665
-local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", { clear = true })
-vim.api.nvim_create_autocmd({ "User" }, {
-  pattern = "CodeCompanion*",
-  group = group,
-  callback = function(request)
-    if
-      request.match == "CodeCompanionChatSubmitted"
-      or request.match == "CodeCompanionContextChanged"
-      or request.match == "CodeCompanionChatCreated"
-      or request.match == "CodeCompanionChatOpened"
-      or request.match == "CodeCompanionChatModel"
-      or request.match == "CodeCompanionChatAdapter"
-      or request.match == "CodeCompanionChatClosed"
-      or request.match == "CodeCompanionChatHidden"
-    then
-      return
-    end
-    local msg
-    msg = "[CodeCompanion] " .. request.match:gsub("CodeCompanion", "")
-    vim.notify(msg, "info", {
-      timeout = 1000,
-      keep = function()
-        return not vim
-          .iter({ "Finished", "Opened", "Hidden", "Closed", "Cleared", "Created" })
-          :fold(false, function(acc, cond)
-            return acc or vim.endswith(request.match, cond)
-          end)
-      end,
-      id = "code_companion_status",
-      title = "Code Companion Status",
-      opts = function(notif)
-        notif.icon = ""
-        if vim.endswith(request.match, "Started") then
-          ---@diagnostic disable-next-line: undefined-field
-          notif.icon = spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-        elseif vim.endswith(request.match, "Finished") then
-          notif.icon = " "
-        end
-      end,
-    })
-  end,
-})
-
 return {
   "olimorris/codecompanion.nvim",
   dependencies = { "saghen/blink.cmp" },
   cmd = "CodeCompanion", -- allow the abbreviation :cc to load the plugin
   cond = vim.g.full_config,
-  config = true,
-  init = function()
-    vim.api.nvim_create_autocmd({ "User" }, {
-      pattern = "CodeCompanionInlineFinished",
-      group = group,
-      callback = function(request)
-        vim.lsp.buf.format({ bufnr = request.buf })
-      end,
-    })
-  end,
   opts = {
     display = {
       chat = {
@@ -111,6 +53,64 @@ return {
       cmd = { adapter = "copilot" },
     },
   },
+  config = function(_, opts)
+    require("codecompanion").setup(opts)
+
+    -- vim.cmd([[cab cc CodeCompanion]]) -- works in visual mode too!
+    -- vim.cmd([[cab cmd CodeCompanionCmd]])
+
+    -- https://github.com/olimorris/codecompanion.nvim/discussions/813#discussioncomment-13081665
+    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+    local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", { clear = true })
+    vim.api.nvim_create_autocmd({ "User" }, {
+      pattern = "CodeCompanion*",
+      group = group,
+      callback = function(request)
+        if
+          request.match == "CodeCompanionChatSubmitted"
+          or request.match == "CodeCompanionContextChanged"
+          or request.match == "CodeCompanionChatCreated"
+          or request.match == "CodeCompanionChatOpened"
+          or request.match == "CodeCompanionChatModel"
+          or request.match == "CodeCompanionChatAdapter"
+          or request.match == "CodeCompanionChatClosed"
+          or request.match == "CodeCompanionChatHidden"
+        then
+          return
+        end
+        local msg
+        msg = "[CodeCompanion] " .. request.match:gsub("CodeCompanion", "")
+        vim.notify(msg, "info", {
+          timeout = 1000,
+          keep = function()
+            return not vim
+              .iter({ "Finished", "Opened", "Hidden", "Closed", "Cleared", "Created" })
+              :fold(false, function(acc, cond)
+                return acc or vim.endswith(request.match, cond)
+              end)
+          end,
+          id = "code_companion_status",
+          title = "Code Companion Status",
+          opts = function(notif)
+            notif.icon = ""
+            if vim.endswith(request.match, "Started") then
+              ---@diagnostic disable-next-line: undefined-field
+              notif.icon = spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            elseif vim.endswith(request.match, "Finished") then
+              notif.icon = " "
+            end
+          end,
+        })
+      end,
+    })
+    vim.api.nvim_create_autocmd({ "User" }, {
+      pattern = "CodeCompanionInlineFinished",
+      group = group,
+      callback = function(request)
+        vim.lsp.buf.format({ bufnr = request.buf })
+      end,
+    })
+  end,
   keys = {
     {
       "<leader>k",
