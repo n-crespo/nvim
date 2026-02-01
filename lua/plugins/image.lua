91cebdf52a6ed41021fe661d09d9614612c851f4
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-01-31 18:46:07 -0800

    chore: remove duplicate autocmds

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,19 +1,3 @@
-local formats =
-  { "png", "jpg", "jpeg", "gif", "bmp", "webp", "tiff", "heic", "avif", "mp4", "mov", "avi", "mkv", "webm", "pdf" }
-
-vim.api.nvim_create_autocmd({ "FileType", "BufReadCmd" }, {
-  desc = "Set image filetypes",
-  pattern = "*." .. table.concat(formats, ",*."),
-  callback = function(e)
-    Snacks.util.bo(e.buf, {
-      filetype = "image",
-      modifiable = false,
-      modified = false,
-      swapfile = false,
-    })
-  end,
-})
-
 return {
   -- this doesn't work in windows terminal but sixel does (used in yazi)
   "folke/snacks.nvim",
