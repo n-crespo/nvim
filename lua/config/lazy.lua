588924ba3c8f6f36d1bcb1d8b8dfdda7ba6e17f3
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-02-03 18:05:13 -0800

    fix: disable eslint

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,7 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind", cond = full_config },
     { import = "lazyvim.plugins.extras.lang.typescript", cond = full_config },
     { import = "lazyvim.plugins.extras.lang.typst", cond = full_config },
-    { import = "lazyvim.plugins.extras.linting.eslint", cond = full_config },
+    -- { import = "lazyvim.plugins.extras.linting.eslint", cond = full_config },
     -- { import = "lazyvim.plugins.extras.dap.core", cond = full_config },
     -- { import = "lazyvim.plugins.extras.ai.copilot-chat", cond = full_config },
     -- { import = "lazyvim.plugins.extras.ui.smear-cursor", cond = full_config },
