34eb6c71a7909a7bf6a127dadd17c0820732d0dd
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-03-14 16:13:45 -0700

    chore: remove unneeded dap keybind

--- lua/plugins/_old/dap.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
-  callback = function()
-    if vim.bo.filetype == "dap-float" then
-      vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = true })
-    end
-  end,
-})
-
-return {}
