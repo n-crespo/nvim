054c098f0a47281afb64696431199722efe5609a
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-03-15 23:09:48 -0700

    chore: remove comment.nvim references

--- after/ftplugin/pvs.lua
+++ after/ftplugin/pvs.lua
@@ -1,7 +1,3 @@
 vim.opt.commentstring = "% %s"
-if not vim.g.vscode then
-  local ft = require("Comment.ft")
-  ft.pvs = "% %s"
-  vim.opt_local.foldmethod = "indent"
-  vim.cmd([[setlocal fdm=indent]])
-end
+vim.opt_local.foldmethod = "indent"
+vim.cmd([[setlocal fdm=indent]])
