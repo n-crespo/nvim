dd1839c984d9e121ff69506b8daac8752c9e9c46
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-06-01 15:53:08 -0700

    perf: disable some neovim builtin plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -7,3 +7,38 @@ require("plugins.explore")
 require("plugins.tabline")
 require("plugins.statusline")
 require("plugins.scroll")
+
+-- disable some builtin plugins that i never use
+local disabled = {
+  "2html_plugin",
+  "bugreport",
+  "compiler",
+  "ftplugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "matchit",
+  -- "matchparen",
+  "netrw",
+  "netrwFileHandlers",
+  "netrwPlugin",
+  "netrwSettings",
+  "optwin",
+  "rplugin",
+  "rrhelper",
+  -- "spellfile_plugin",
+  "synmenu",
+  "tar",
+  "tarPlugin",
+  "tohtml",
+  "tutor",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+}
+
+for _, plugin in ipairs(disabled) do
+  vim.g["loaded_" .. plugin] = 1
+end
