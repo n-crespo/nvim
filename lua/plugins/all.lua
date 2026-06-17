2baeed67951e5969b0210a226bf258ede334abfe
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-06-17 14:47:35 -0600

    perf: disable oxfmt

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -2238,17 +2238,17 @@ return {
   },
 
   -- conform
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.formatters_by_ft = opts.formatters_by_ft or {}
-      for _, ft in ipairs(supported) do
-        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
-        table.insert(opts.formatters_by_ft[ft], "oxfmt")
-      end
-    end,
-  },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     opts.formatters_by_ft = opts.formatters_by_ft or {}
+  --     for _, ft in ipairs(supported) do
+  --       opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+  --       table.insert(opts.formatters_by_ft[ft], "oxfmt")
+  --     end
+  --   end,
+  -- },
 
   {
     "ice345/markdown-table-wrap.nvim",
