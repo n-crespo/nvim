a46084efe2c1f90662a8e1038ded9a355c38a029
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-07-01 13:59:51 -0600

    chore: switch python formatter to black

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -1523,7 +1523,7 @@ return {
         ["json"] = { "jq" },
         ["typst"] = { "prettypst" },
         ["c"] = { "clang-format" },
-        ["python"] = { "ruff" },
+        ["python"] = { "black" },
         ["tex"] = { "tex-fmt" },
         -- ["zsh"] = { "beautysh" },
       },
