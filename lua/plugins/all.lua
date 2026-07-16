d3117882804b96c665e81b1b6226e4d7210b8c8a
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-07-16 13:51:32 -0600

    feat: add incline.nvim

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -2257,4 +2257,54 @@ return {
       { "<leader>t", "<cmd>MarkdownTableTogglePreview<cr>", desc = "Toggle table preview", ft = "markdown" },
     },
   },
+  {
+    "b0o/incline.nvim",
+    enabled = true,
+    event = "LazyFile",
+    dependencies = {
+      { "nvim-mini/mini.icons" },
+      {
+        "folke/snacks.nvim",
+        opts = {
+          zen = {
+            on_close = function()
+              require("incline").enable()
+            end,
+            on_open = function()
+              require("incline").disable()
+            end,
+          },
+        },
+      },
+    },
+    opts = {
+      debounce_threshold = { falling = 20, rising = 0 },
+      window = {
+        zindex = 30,
+        options = { buftype = "nofile", wrap = false, buflisted = false },
+        margin = { horizontal = 0, vertical = 1 },
+        overlap = { tabline = false, winbar = false },
+        width = "fit",
+        winhighlight = { Normal = "Visual" },
+      },
+      hide = {
+        cursorline = "focused_win",
+        only_win = false,
+      },
+      render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        if filename == "" then
+          filename = "[No Name]"
+        end
+
+        local ft_icon, ft_color = require("mini.icons").get("file", filename)
+        local modified = vim.bo[props.buf].modified
+        return {
+          ft_icon and { (ft_icon or "") .. " ", group = ft_color, guibg = "none" } or "",
+          { filename, gui = modified and "bold" or "none" },
+          { modified and " " or "" },
+        }
+      end,
+    },
+  },
 }
